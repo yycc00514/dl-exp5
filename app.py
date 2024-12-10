d0587631803b7e700b833a45629ef1e3652465af
yycc00514 <162726083+yycc00514@users.noreply.github.com>
2024-12-10 16:11:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -61,6 +61,7 @@ def delTempFolder():
 
 
 def runStreamlit():
+    print(os.getcwd())
     # 初始化 session_state
     if 'detectDone' not in st.session_state:
         st.session_state.detectDone = False
